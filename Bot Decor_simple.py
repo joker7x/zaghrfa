757a6c94f4dcd26258d26e884144327ba274ddad
joker7x <59514975+joker7x@users.noreply.github.com>
2023-01-15 04:25:02 +0200

    Add files via upload

---
+++ Bot Decor_simple.py
@@ -0,0 +1,31 @@
+from requests import *
+import re
+from telebot import *
+from telebot.types import (
+InlineKeyboardMarkup as Mk ,
+InlineKeyboardButton as btn)
+bot = TeleBot(input("Token : "))
+dev = Mk().add(btn(text='• Developer •',url='t.me/DevNoxi'))
+@bot.message_handler(commands=['start'])
+def start(msg):
+	bot.send_message(msg.chat.id,'''• اهلا بك 
+في بوت الزخرفة ؛)
+- قم بأرسال اسمك بـ اللغة العربي او الانجليزي !''',reply_markup=dev)
+@bot.message_handler(content_types=['text'])
+def main(msg):
+	ii = 0;text = msg.text
+	try:
+		res = get(f"http://xn--ogbjjc1f.com/inc/php/fancy-fonts.php?q={text}").text
+		for i in range(22):
+			ii += 1
+			x = re.findall(f'input type="text" class="ResultName" id="username{ii}" value="(.*?)" readonly',res)
+			if x == []:pass
+			else:
+				bot.send_message(msg.chat.id,f'`{x[0]}`',
+				parse_mode='Markdown')
+		bot.reply_to(msg,f"""- تم زخرفة الإسم {text} .
+- أرسل أسم أخر للزخرفة ..""",
+		reply_markup=dev)
+#			print(x[0])
+	except:pass
+bot.infinity_polling()
